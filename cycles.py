fb00b327676732a54c3fc54bc2876ec26adf6fe0
bolshakov863 <ivan.bolshakov113@gmail.com>
2024-10-03 20:50:53 +0300

    add new program

--- cycles.py
+++ cycles.py
@@ -4,3 +4,28 @@
 
 # for i in 'Python':
 #     print(i)
+
+
+#for number in range(1, 6):
+#   print(number)
+
+
+#a = 1
+#while a < 10:
+#    print('Цикл выполнился', a, 'раз(а)')
+#    a = a+1
+#print('Цикл окончен')
+
+
+#a = 1
+#while a < 5:
+#   print('условие верно')
+#   a = a + 1
+#else:
+#   print('условие неверно')
+
+
+#n = int(input())
+#while n != 0:
+#     print(n + 5)
+#     n = int(input())
