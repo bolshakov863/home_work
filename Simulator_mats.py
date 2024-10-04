a16896a8a4b0bf9bb21f362fdb091e3dfeb3e538
bolshakov863 <ivan.bolshakov113@gmail.com>
2024-10-04 18:49:52 +0300

    add new program

--- Simulator_mats.py
+++ Simulator_mats.py
@@ -1,76 +1,122 @@
-class Hitbox:
-    def __init__(self, x, y, width, height):
-        self.__x = x
-        self.__y = y
-        self.__set_width(width)
-        self.__set_height(height)
 
-    def __get_x(self):
-        return self.__x
+#class Hitbox:
+    #def __init__(self, x, y, width, height):
+        #self.__x = x
+        #self.__y = y
+        #self.__set_width(width)
+        #self.__set_height(height)
 
-    def __set_x(self, x):
-        self.__x = x
+    #def __get_x(self):
+        #return self.__x
 
-    def __get_y(self):
-        return self.__y
+    #def __set_x(self, x):
+        #self.__x = x
 
-    def __set_y(self, y):
-        self.__y = y
+    #def __get_y(self):
+       # return self.__y
 
-    def __get_width(self):
-        return self.__get_width
+    #def __set_y(self, y):
+      #  self.__y = y
 
-    def __set_width(self, width):
-        if width < 0:
-            width = 0
-        self.__width = width
+    #def __get_width(self):
+       # return self.__get_width
 
-    def __get_height(self):
-        return self.__get_height
+    #def __set_width(self, width):
+        #if width < 0:
+            #width = 0
+        #self.__width = width
 
-    def __set_height(self, height):
-        if height < 0:
-            height = 0
-        self.__height = height
+    #def __get_height(self):
+       #return self.__get_height
 
-    def moveto(self, x, y):
-        self.__set_x(x)
-        self.__set_y(y)
+    #def __set_height(self, height):
+        #if height < 0:
+            #height = 0
+        #self.__height = height
 
-    def move(self, dx, dy):
-        self.__set_x(dx + self.__get_x())
-        self.__set_y(dy + self.__get_y())
+    #def moveto(self, x, y):
+        #self.__set_x(x)
+        #self.__set_y(y)
 
-    def __get_top(self):
-        return self.y
+    #def move(self, dx, dy):
+        #self.__set_x(dx + self.__get_x())
+        #self.__set_y(dy + self.__get_y())
 
-    def __get_bottom(self):
-        return self.y + self.height
+    #def __get_top(self):
+        #return self.y
 
-    def __get_left(self):
-        return self.x
+    #def __get_bottom(self):
+        #return self.y + self.height
 
-    def intersects(self, other):
-        if self.left > other.right:
-            return False
-        elif self.top > other.bottom:
-            return False
-        elif self.right > other.left:
-            return False
-        elif self.bottom > other.top:
-            return False
-        else:
-            return True
+    #def __get_left(self):
+        #return self.x
 
-x = property(__get_x, __set_x)
-y = property(__get_y, __set_y)
-width = property(__get_width, __set_width)
-height = property(__get_height, __set_height)
+    #def intersects(self, other):
+        #if self.left > other.right:
+            #return False
+        #elif self.top > other.bottom:
+           #return False
+        #elif self.right > other.left:
+           # return False
+       # elif self.bottom > other.top:
+            #return False
+       # else:
+           # return True
 
-left = property(__get_left)
-right = property(__get_right)
-top = property(__get_top)
-bottom = property(__get_bottom)
+#x = property(__get_x, __set_x)
+#y = property(__get_y, __set_y)
+#width = property(__get_width, __set_width)
+#height = property(__get_height, __set_height)
 
-def __str__(self):
-    return f"{self.__x=},{self.__y=},{self.__width=},{self.__height=}"
+#left = property(__get_left)
+#right = property(__get_right)
+#top = property(__get_top)
+#bottom = property(__get_bottom)
+
+#def __str__(self):
+    #return f"{self.__x=},{self.__y=},{self.__width=},{self.__height=}"
+
+from hitbox import Hitbox
+SIZE = 100
+def __init__(self, canvas, x, y, model='Т-14 Армата', ammo=100, speed = 10):
+    self.__hitbox = Hitbox(x, y, Tank.SIZE, Tank.SIZE)
+
+    def __update_hitbox(self):
+        self.__hitbox.moveto(self.x, self.y)
+
+    def forward(self):
+        if self.fuel > 0:
+            self.y  += -self.speed
+            self.__update_hitbox()
+            self.fuel -= 1
+            self.repaint()
+
+    def backward(self):
+        if self.fuel > 0:
+            self.y  += self.speed
+            self.__update_hitbox()
+            self.fuel -= 1
+            self.repaint()
+
+    def left(self):
+        if self.fuel > 0:
+            self.y += self.speed
+            self.__update_hitbox()
+            self.fuel -= 1
+            self.repaint()
+
+    def right(self):
+        if self.fuel > 0:
+            self.y += self.speed
+            self.__update_hitbox()
+            self.fuel -= 1
+            self.repaint()
+
+    def intersects(self)
+
+
+
+
+
+
+
--- victorina.py
+++ victorina.py
@@ -1,39 +1,39 @@
-from random import choice
+#from random import choice
 
-student = input('Представьтесь, пожалуйста: ')
-try:
-    level = int(input('Выберете уровень сложности 1 - 3: '))
-except:
-    level = 1
-    print("Установлен первый уровень сложности")
+#student = input('Представьтесь, пожалуйста: ')
 
-print(f'Хорошо, {student}. Тебе викторина по истории!')
+#level = int(input('Выберете уровень сложности 1 - 3: '))
+#except:
+    #level = 1
+    #print("Установлен первый уровень сложности")
+
+#print(f'Хорошо, {student}. Тебе викторина по истории!')
 
 
-questions = {
-    1: [('В каком году началась Великая Отечественная война?', '1941'),
-        ('Кто был первым президентом США?', 'Джордж Вашингтон')],
-    2: [('Кто был первым императором Римской империи?', 'Октавион Август'),
-        ('В каком году распался Советский Союз?', '1991')],
-    3: [('Когда была подписана Магна Карта?', '1215'),
-        ('Кто был фараоном когда строили пирамиды?', 'Хеопс')]
-}
+#questions = {
+    #1: [('В каком году началась Великая Отечественная война?', '1941'),
+     #   ('Кто был первым президентом США?', 'Джордж Вашингтон')],
+   # 2: [('Кто был первым императором Римской империи?', 'Октавион Август'),
+      #  ('В каком году распался Советский Союз?', '1991')],
+   # 3: [('Когда была подписана Магна Карта?', '1215'),
+     #   ('Кто был фараоном когда строили пирамиды?', 'Хеопс')]
+#}
 
-points = 0
-for i in range(3):
-    question, correct_answer = choice(questions[level])
-    print(f'{student}, {question} ', end='')
-    student_answer = input().strip().lower()
-    if student_answer == correct_answer.lower():
-        points += 1
-        print("Правильно!")
-    else:
-        print(f'Не правильно. Правильный ответ {correct_answer}!')
+#points = 0
+#for i in range(3):
+ #   question, correct_answer = choice(questions[level])
+#    print(f'{student}, {question} ', end='')
+#    student_answer = input().strip().lower()
+#    if student_answer == correct_answer.lower():
+#        points += 1
+#        print("Правильно!")
+#    else:
+#        print(f'Не правильно. Правильный ответ {correct_answer}!')
 
-if points == 3:
-    print(f'Ты историк, {student}!')
-elif points == 2:
-    print(f'Хорошо, {student}, но можно глубже изучать.')
-else:
-    print(f'История не твоя сильная сторона, {student}.')
+#if points == 3:
+  #  print(f'Ты историк, {student}!')
+#elif points == 2:
+   # print(f'Хорошо, {student}, но можно глубже изучать.')
+#else:
+#    print(f'История не твоя сильная сторона, {student}.')
 
