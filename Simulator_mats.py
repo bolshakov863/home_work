26d56933712d0f519754743f8dc06ee7ed1543b0
bolshakov863 <ivan.bolshakov113@gmail.com>
2024-10-04 19:10:33 +0300

    add new program

--- Simulator_mats.py
+++ Simulator_mats.py
@@ -76,46 +76,82 @@
 #def __str__(self):
     #return f"{self.__x=},{self.__y=},{self.__width=},{self.__height=}"
 
-from hitbox import Hitbox
-SIZE = 100
-def __init__(self, canvas, x, y, model='Т-14 Армата', ammo=100, speed = 10):
-    self.__hitbox = Hitbox(x, y, Tank.SIZE, Tank.SIZE)
+#from hitbox import Hitbox
+#SIZE = 100
+#def __init__(self, canvas, x, y, model='Т-14 Армата', ammo=100, speed = 10):
+ #   self.__hitbox = Hitbox(x, y, Tank.SIZE, Tank.SIZE)
 
-    def __update_hitbox(self):
-        self.__hitbox.moveto(self.x, self.y)
+#    def __update_hitbox(self):
+#        self.__hitbox.moveto(self.x, self.y)
 
-    def forward(self):
-        if self.fuel > 0:
-            self.y  += -self.speed
-            self.__update_hitbox()
-            self.fuel -= 1
-            self.repaint()
+#    def forward(self):
+#        if self.fuel > 0:
+#            self.y  += -self.speed
+#            self.__update_hitbox()
+#            self.fuel -= 1
+ #           self.repaint()
 
-    def backward(self):
-        if self.fuel > 0:
-            self.y  += self.speed
-            self.__update_hitbox()
-            self.fuel -= 1
-            self.repaint()
+#    def backward(self):
+ #       if self.fuel > 0:
+ #           self.y  += self.speed
+ #           self.__update_hitbox()
+ #           self.fuel -= 1
+ #           self.repaint()
 
-    def left(self):
-        if self.fuel > 0:
-            self.y += self.speed
-            self.__update_hitbox()
-            self.fuel -= 1
-            self.repaint()
+ #   def left(self):
+ #       if self.fuel > 0:
+ #           self.y += self.speed
+ #           self.__update_hitbox()
+#            self.fuel -= 1
+ #           self.repaint()
+#
+ #   def right(self):
+ #       if self.fuel > 0:
+#            self.y += self.speed
+#            self.__update_hitbox()
+ #           self.fuel -= 1
+ #           self.repaint()
+#
+#    def intersects(self, others_tank):
+ #       pass
 
-    def right(self):
-        if self.fuel > 0:
-            self.y += self.speed
-            self.__update_hitbox()
-            self.fuel -= 1
-            self.repaint()
+ #   def intersects(self, others_tank):
+ #       return self.__hitbox.intersects(other_tank.__hitbox)
 
-    def intersects(self)
+ #   def check_collision():
+ #       if player.intersects(enemy):
+ #           print('Танки столкнулись')
+#
+#    def key_press():
+ #       if event.keycode ==KEY_W:
+#            player.forward()
+ #       if event.keycode ==KEY_A:
+ #           player.left()
+#        if event.keycode ==KEY_S:
+#            player.backward()
+#        if event.keycode ==KEY_D:
+ #           player.right()
 
+#        check_collision()
 
+ #   def __init__(self, canvas, x, y, model='Т-14 Армата', ammo=100, speed=10):
+ #       Tank.__count += 1
+#        self.__hitbox = Hitbox(x, y, Tank.__SIZE, Tank.__SIZE)
+#        self.__speed = speed
+ #       self.__canvas = canvas
+ #       self.__model = model
+#        self.__fuel = 100
+#        self.__hp = 100
+ #       self.__xp = 0
+#        self.__ammo = ammo
+ #       self.__x = x
+ #       self.__y = y
+ #       if self.__x < 0:
+ #           self.__x = 0
+ #       if self.__y < 0:
+ #           self.__y = 0
 
+#        self.create()
 
 
 
