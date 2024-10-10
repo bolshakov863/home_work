dc5ecc1f33038bc41a9f126dff570fc4937ffb4e
bolshakov863 <ivan.bolshakov113@gmail.com>
2024-10-10 17:24:48 +0300

    add new file

---
+++ celsius_faringheit.py
@@ -0,0 +1,22 @@
+class Temperature:
+
+
+    def __init__(self, celsius):
+        self.__celsius = celsius
+
+    def to_fahrenheit(self, fahrenheit):
+        self.fahrenheit = self.__celsius * 1.8 + 32
+        print(fahrenheit)
+
+    def get_celsius(self):
+        return self.__celsius
+        print(celsius)
+
+
+
+
+
+
+
+
+
