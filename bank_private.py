aaa398bfca8f3bfe8e93728cdc667a16f8660f4b
bolshakov863 <ivan.bolshakov113@gmail.com>
2024-10-10 16:45:25 +0300

    add new programs

--- bank_private.py
+++ bank_private.py
@@ -1,20 +1,20 @@
 class BankAccount:
     def __init__(self, balance):
-        self.__balance = balance
+        self.__balance = balance # приватная переменная
 
     def deposit(self, amount):
-        if amount > 0:
-            self.__balance += amount
+        if amount > 0:     # если amount больше 0
+            self.__balance += amount   # прибавить к self.__balance amount
 
     def withdraw(self, amount):
-        if 0 < amount <= self.__balance:
-            self.__balance -=amount
-        else:
-            print('Недостаточно средств')
+        if 0 < amount <= self.__balance:  # если amount больше 0 ,но меньше self.__balance
+            self.__balance -= amount   # вычесть из self.__balance amount
+        else: # или
+            print('Недостаточно средств') # написать Недостаточно средств
 
     def get_balance(self):
-        return self.__balance
+        return self.__balance  # вернуть self.__balance
 
-account = BankAccount(1000)
-account.deposit(500)
-print(account.get_balance())
+account = BankAccount(1000) # переменная account
+account.deposit(500) # переменная account.deposit
+print(account.get_balance()) # написать account.get_balance
