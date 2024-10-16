4f996b196fc134f9849fa64d5cd7ecaa6ebf41bc
bolshakov863 <ivan.bolshakov113@gmail.com>
2024-10-16 21:21:49 +0300

    add new words

--- hb/main1.py
+++ hb/main1.py
@@ -1,17 +1,22 @@
 from hitbox import Hitbox
 
 hb1 = Hitbox(x=0, y=100, width=100, height=100)
-hb2 = Hitbox(x=150, y=80, width=100, height=100)
+hb2 = Hitbox(x=50, y=50, width=100, height=100)
+hb3 = Hitbox(x=100, y=100, width=100, height=100)
 
 
 print(f'Верхняя граница hb1:{hb1.top}'
-      f'Верхняя граница hb2:{hb2.top}')
+      f'Верхняя граница hb2:{hb2.top}'
+      f'Верхняя граница hb3:{hb3.top}')
 print(f'Нижняя граница hb1:{hb1.bottom}'
-      f'Нижняя граница hb2:{hb2.bottom}')
+      f'Нижняя граница hb2:{hb2.bottom}'
+      f'Нижняя граница hb3:{hb3.bottom}')
 print(f'Левая граница hb1:{hb1.left}'
-      f'Левая граница hb2:{hb2.left}')
+      f'Левая граница hb2:{hb2.left}'
+      f'Левая граница hb3:{hb3.left}')
 print(f'Правая граница hb1:{hb1.right}'
-      f'Правая граница hb2:{hb2.right}')
+      f'Правая граница hb2:{hb2.right}'
+      f'Правая граница hb3:{hb3.right}')
 
 intersection = hb1.intersects(hb2)
 print(intersection)
