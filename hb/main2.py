37dabb6f0bf9e438043f4dbbaaa46cf3d77fdffe
bolshakov863 <ivan.bolshakov113@gmail.com>
2024-10-17 16:25:39 +0300

    add new words

--- hb/main2.py
+++ hb/main2.py
@@ -5,9 +5,9 @@ hb2 = Hitbox(x=150, y=100, width=100, height=100)
 hb3 = Hitbox(x=300, y=100, width=100, height=100)
 
 
-intersection1 = hb1.intersects(hb1)
-intersection2 = hb2.intersects(hb2)
-intersection3 = hb3.intersects(hb3)
+intersection1 = hb1.intersects(hb2)
+intersection2 = hb2.intersects(hb3)
+intersection3 = hb1.intersects(hb3)
 print(intersection1)
 print(intersection2)
 print(intersection3)
