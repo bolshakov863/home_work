5583c73e3b067202dc21538001df53d60a8e4629
bolshakov863 <ivan.bolshakov113@gmail.com>
2024-12-27 21:29:25 +0300

    add hj

--- 3/world.py
+++ 3/world.py
@@ -19,6 +19,21 @@ HEIGHT = SCREEN_HEIGHT * 4
 _canvas = None
 _map = []
 
+def load_map(file_name):
+    global map
+
+    _map = []
+    with open(file_name) as f:
+        i = 0
+        for line in f:
+            blocks = line.split()
+            row = []
+            for j in range(len(blocks)):
+                cell = _Cell(j * BLOCK_SIZE, i * BLOCK_SIZE, _canvas, blocks[j])
+            _map.append(row)
+            i+=1
+    f.close()
+
 def get_block(row, col):
     if row < 0 or col < 0 or row >= get_rows() \
             or col >= get_cols():
@@ -82,9 +97,8 @@ def create_map(rows = 20, cols = 20):
 def initialize(canv):
     global _canvas
     _canvas = canv
-    create_map(20, 20)
-
-
+   # create_map(25, 25)
+    load_map('./map/1.tmap.py')
 
 def set_camera_xy(x, y):
     global _camera_x, _camera_y
